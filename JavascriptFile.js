0be67b928caac226dc63b3618ab8a673b1bc6497
unknown <anthony.meadows2000@outlook.com>
2023-11-13 08:25:28 -0700

    js.js has new git cmds

---
+++ JavascriptFile.js
@@ -0,0 +1,29 @@
+//This is practice for git - and git hub
+
+//Commands for git
+// git init = initialize project for use 
+//   this folder we are initing, is the repo
+
+// git add . = add all changes `to be saved` (in the repo)
+// git add (filename) = add single file `to be saved`
+
+// git commit -m "message"  = `savechanges` with Message
+
+// git push origin (branch) = push changes to github (branch)
+// git pull origin (branch) = pull changes from github
+
+// git status = check status of changes
+
+// git log = see all previos saved changes
+
+// git checkout -b new-branch = create a new branch
+// git checkout (commit hash) = travel back to old commit
+
+//for the fun of it
+
+function addOne(num) {
+  return num+=1
+}
+
+var thisNumber = addOne(5)
+console.log(thisNumber)
